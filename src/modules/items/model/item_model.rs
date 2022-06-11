27070f059fd44028d16821bb1690e1d19e6ed5ae
Jeddi212 <jedediahf51@gmail.com>
2022-06-12 00:44:00 +0700

    add items constructor

--- src/modules/items/model/item_model.rs
+++ src/modules/items/model/item_model.rs
@@ -1,5 +1,7 @@
 use serde::{Serialize, Deserialize};
 use diesel::Queryable;
+
+use super::super::dto::item_dto::*;
 use crate::schema::*;
 
 #[derive(Serialize, Deserialize)]
@@ -9,4 +11,15 @@ pub struct Items {
     pub title: String,
     pub checked: bool,
     pub list_id: i32
+}
+
+impl From<ItemDTO> for Items {
+    fn from(i: ItemDTO) -> Self {
+        Self {
+            id: i.id,
+            title: i.title,
+            checked: i.checked,
+            list_id: i.list_id
+        }
+    }
 }
